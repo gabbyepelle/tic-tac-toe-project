bf92a680efd308e9752b0aefd02416003de0af60
Gabrielle Epelle <gabbyepelle@gmail.com>
2023-07-01 15:14:17 -0400

    reorganized modules

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let player_moves = 0;
+let cpu_moves = 0;
+
 const Gameboard = (()=>{
     const createBoard = function(){
         const board = new Array(9);
@@ -16,17 +19,27 @@ const displayBoard = function(board){
                 count++
         }
 }
-const makeMove = function(marker, board){
+
+return {createBoard, displayBoard}
+} 
+)()
+
+const Player = (name, marker) =>{
+    const makeMove = function(board){
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((box)=>{
         box.addEventListener("click", ()=>{
             //check if box is empty
-          //add marker to box
+            //add marker to box
            if(!board[box.dataset.index]){
+            //if the board is null at that position
             box.innerText = `${marker}`;
             board[box.dataset.index] = marker;
-            console.log(board)
-           }else{
+            // console.log(board)
+            // player_moves++;
+            // console.log(player_moves)
+           }
+           else{
             alert("space unavailable")
            }
             
@@ -35,7 +48,7 @@ const makeMove = function(marker, board){
 
 }
 
-const randomMove = function(marker, board){
+const randomMove = function(board){
     const rand = Math.floor(Math.random() * (board.length -1))
     const boxes = document.querySelectorAll(".box");
    
@@ -46,27 +59,53 @@ const randomMove = function(marker, board){
                 console.log("found it")
                 box.innerText = `${marker}`;
                 board[rand] = marker;
+                cpu_moves ++;
                 console.log(board)
                     }
             }
         }else{
-            randomMove(marker, board)
+            randomMove(board)
         }
     }
-         
+    return{makeMove, randomMove}
+}
+
+
+//create different players
+//play game, alternating player turns
 //check game winner
+//check if board is full
+
+const gameController = (()=>{
+    const gameboard = Gameboard.createBoard();
+    Gameboard.displayBoard(gameboard);//should this be passed in? idk
+    const player1 = Player("player1", "X")
+    const player2 = Player("player2", "O")
+    const players = [player1, player2];
+    let activePlayer = players[0];
+    const switchPlayerTurn = ()=>{
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+        alert("switch")
+    }
+    const getActivePlayer = ()=> activePlayer;
+
+const playRound = ()=>{
+    getActivePlayer().makeMove(gameboard);
+    switchPlayerTurn();
+    //create functions to check for game winner and whether board is full
+    }
+
+return{
+     playRound, getActivePlayer
+    }
+
+})()
+
+
+gameController.playRound();
 
 
 
-return {createBoard, displayBoard, makeMove, randomMove}
-} 
-)()
-
-
-const gameboard = Gameboard.createBoard();
-Gameboard.displayBoard(gameboard);
-Gameboard.makeMove("X",gameboard);
-Gameboard.randomMove("O", gameboard);
 
 
 
